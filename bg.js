17d6745ed53858feff1b118a70b8fe41ed63ed48
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2020-02-10 00:40:23 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -3,7 +3,7 @@ const IMG_NUMBER = 4;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `img/${imgNumber+1}.jpg`;
+    image.src = `${imgNumber+1}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
 }
